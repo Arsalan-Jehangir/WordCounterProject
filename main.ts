914b5a54de6bf1027bc2f6ca93967887dc3c956c
Arsalan-Jehangir <142014329+Arsalan-Jehangir@users.noreply.github.com>
2023-11-07 11:48:05 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+import inquirer from 'inquirer'
+
+const counter = (text:string) =>
+     text.replace(/\s/g,"").length
+
+    
+async function startWordCount(counter:(text:string)=>number){
+    do{
+        let response = await inquirer.prompt({
+            type: "input",
+            message: "Write Your Text Here...",
+            name: "text"
+        })
+        console.log(counter(response.text));
+    }
+    while(true)
+}
+
+startWordCount(counter);
